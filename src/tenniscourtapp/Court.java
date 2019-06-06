70b93e74fd3085bc6f2d0de4ab5c02c3452624a3
winnann20 <winnann20@SHS-PLAB-86780.shs.isd.issaquah.wednet.edu>
2019-06-06 08:45:30 -0700

    basic GUI and court

---
+++ src/tenniscourtapp/Court.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tenniscourtapp;
+
+import java.awt.Point;
+/**
+ *
+ * @author winnann20
+ */
+public class Court {
+    private String name;
+    private Point location;
+    private int numCourts;
+    private boolean available;
+    private String cost;
+    private String[] amenities;
+
+    public Court(String name, Point location, int numCourts, boolean available, String cost, String[] amenities) {
+        this.name = name;
+        this.location = location;
+        this.numCourts = numCourts;
+        this.cost = cost;
+        this.amenities = amenities;
+        this.available = true;
+    }
+}
